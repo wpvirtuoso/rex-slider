7cd4d5da6a1747be3b47e1c57c2c9df468cdca3a
wpvirtouso <36991873+wpvirtuoso@users.noreply.github.com>
2023-01-19 23:12:42 +0500

    slider style

--- public/class-rex-slider-public.php
+++ public/class-rex-slider-public.php
@@ -20,262 +20,271 @@
  * @subpackage Rex_Slider/public
  * @author     Wp Virtuoso <zaxrana.pk@gmail.com>
  */
-class Rex_Slider_Public
-{
+class Rex_Slider_Public {
 
-    /**
-     * The ID of this plugin.
-     *
-     * @since  1.0.0
-     * @access private
-     * @var    string    $plugin_name    The ID of this plugin.
-     */
-    private $plugin_name;
 
-    /**
-     * The version of this plugin.
-     *
-     * @since  1.0.0
-     * @access private
-     * @var    string    $version    The current version of this plugin.
-     */
-    private $version;
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since  1.0.0
+	 * @access private
+	 * @var    string    $plugin_name    The ID of this plugin.
+	 */
+	private $plugin_name;
 
-    /**
-     * Initialize the class and set its properties.
-     *
-     * @since 1.0.0
-     * @param string $plugin_name The name of the plugin.
-     * @param string $version     The version of this plugin.
-     */
-    public function __construct( $plugin_name, $version )
-    {
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since  1.0.0
+	 * @access private
+	 * @var    string    $version    The current version of this plugin.
+	 */
+	private $version;
 
-        $this->plugin_name = $plugin_name;
-        $this->version = $version;
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since 1.0.0
+	 * @param string $plugin_name The name of the plugin.
+	 * @param string $version     The version of this plugin.
+	 */
+	public function __construct( $plugin_name, $version ) {
 
-    }
+		$this->plugin_name = $plugin_name;
+		$this->version     = $version;
 
-    /**
-     * Register the stylesheets for the public-facing side of the site.
-     *
-     * @since 1.0.0
-     */
-    public function enqueue_styles()
-    {
+	}
 
-        /**
-         * This function is provided for demonstration purposes only.
-         *
-         * An instance of this class should be passed to the run() function
-         * defined in Rex_Slider_Loader as all of the hooks are defined
-         * in that particular class.
-         *
-         * The Rex_Slider_Loader will then create the relationship
-         * between the defined hooks and the functions defined in this
-         * class.
-         */
-        wp_enqueue_style(
-            $this->plugin_name, plugin_dir_url(__FILE__) . 'css/rex-slider-public.css', array(),
-            $this->version, 'all'
-        );
-        wp_enqueue_style('css', plugin_dir_url(__FILE__). 'flexslider/flexslider.css');
+	/**
+	 * Register the stylesheets for the public-facing side of the site.
+	 *
+	 * @since 1.0.0
+	 */
+	public function enqueue_styles() {
 
-    }
-    /**
-     * Register the JavaScript for the public-facing side of the site.
-     *
-     * @since 1.0.0
-     */
-    public function enqueue_scripts()
-    {
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Rex_Slider_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Rex_Slider_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+		wp_enqueue_style(
+			$this->plugin_name,
+			plugin_dir_url( __FILE__ ) . 'css/rex-slider-public.css',
+			array(),
+			$this->version,
+			'all'
+		);
+		wp_enqueue_style( 'css', plugin_dir_url( __FILE__ ) . 'flexslider/flexslider.css' );
 
-        /**
-         * This function is provided for demonstration purposes only.
-         *
-         * An instance of this class should be passed to the run() function
-         * defined in Rex_Slider_Loader as all of the hooks are defined
-         * in that particular class.
-         *
-         * The Rex_Slider_Loader will then create the relationship
-         * between the defined hooks and the functions defined in this
-         * class.
-         */
-        wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . 'js/rex-slider-public.js', array( 'jquery' ), $this->version, false);
-        wp_enqueue_script('jquey-min', plugin_dir_url(__FILE__) . 'flexslider/jquery.flexslider-min.js');
+	}
+	/**
+	 * Register the JavaScript for the public-facing side of the site.
+	 *
+	 * @since 1.0.0
+	 */
+	public function enqueue_scripts() {
 
-    }
-    /**
-     * Function registers slider post type
-     *
-     * @since 1.0.0
-     */
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Rex_Slider_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Rex_Slider_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/rex-slider-public.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script( 'jquey-min', plugin_dir_url( __FILE__ ) . 'flexslider/jquery.flexslider-min.js' );
 
-    public function rex_slider_postype()
-    {
-        $args = array(
-        'public' => true,
-        'label' => 'Rex Slides',
-        'singular_label' => 'Add Slides',
-        'capability_type'     => 'post',
-        'supports' => array(
-        'title',
-        'thumbnail',
-        )
+	}
+	/**
+	 * Function registers slider post type
+	 *
+	 * @since 1.0.0
+	 */
 
-        );
-        register_post_type('rex_slides', $args);
-    }
-    /**
-     * Regitser slide taxonomy
-     *
-     * @since 1.0.0
-     */
+	public function rex_slider_postype() {
+		$labels = array(
+			'name'          => 'Rex Slides',
+			'singular_name' => 'Slide',
+			'search_items'  => 'Search Slides',
+			'all_items'     => 'All Slides',
+			'edit_item'     => 'Edit Slide',
+			'add_new_item'  => 'Add new Slide',
+			'update_item'   => 'Update slider',
+			'not_found'     => 'No sliders found',
+		);
 
-    public function rex_slider_taxonomy()
-    {
-        $labels = array (
-        'name' => 'Sliders',
-        'singular_name' => 'Slider',
-        'search_items' => 'Search Sliders',
-        'all_items'   => 'All Sliders',
-        'add_new_item' => 'Add new Slider',
-        'update_item' => 'Update slider',
-        'not_found' => 'No sliders found'
-        );
+		$args = array(
+			'public'   => true,
+			'labels'   => $labels,
+			'supports' => array(
+				'title',
+				'thumbnail',
+			),
 
-        $args = array(
-        'public' => true,
-        'labels' => $labels,
-        'query_var' => true,
-        'show_ui' => true,
-        'show_admin_column' => true,
-        'hierarchical' => true
+		);
+		register_post_type( 'rex_slides', $args );
+	}
+	/**
+	 * Regitser slide taxonomy
+	 *
+	 * @since 1.0.0
+	 */
 
-        );
-        register_taxonomy('rex_slider', 'rex_slides', $args);
+	public function rex_slider_taxonomy() {
+		$labels = array(
+			'name'          => 'Sliders',
+			'singular_name' => 'Slider',
+			'search_items'  => 'Search Sliders',
+			'all_items'     => 'All Sliders',
+			'add_new_item'  => 'Add new Slider',
+			'edit_item'     => 'Edit slider',
+			'update_item'   => 'Update slider',
+			'not_found'     => 'No sliders found',
+		);
 
-    }
-    /**
-     * Adds metabox for rex_slider post type
-     *
-     * @since 1.0.0
-     */
+		$args = array(
+			'public'            => true,
+			'labels'            => $labels,
+			'query_var'         => true,
+			'show_ui'           => true,
+			'show_admin_column' => true,
+			'hierarchical'      => true,
 
-    public function rex_slider_metabox()
-    {
-        add_meta_box(
-            'rex_slider_mb',
-            'Rex Slider options',
-            array($this, 'rex_slider_mb_form'),
-            'rex_slides'
-        );
-    }
-    /**
-     * Adds form fields to meta box
-     *
-     * @since 1.0.0
-     */
+		);
+		register_taxonomy( 'rex_slider', 'rex_slides', $args );
 
-    public function rex_slider_mb_form()
-    {
+	}
+	/**
+	 * Adds metabox for rex_slider post type
+	 *
+	 * @since 1.0.0
+	 */
 
-        global $post;
-        // Get Value of Fields From Database
-        $rex_heading = get_post_meta($post->ID, 'rex_heading', true);
-        $rex_subheading = get_post_meta($post->ID, 'rex_subheading', true);
-        ?>
-    <div class="row">
-        <div class="label"><b>Slider Heading</div>
-        <div class="fields">
-            <textarea style="width:50%" name="rex_heading"><?php echo $rex_heading; ?></textarea>
-    </div>
-    </div>
-    <br>
-    <div class="row">
-        <div class="label"><b>Slider Sub heading</b></div>
-        <div class="fields">
-            <textarea style="width:50%" name="rex_subheading"><?php echo $rex_subheading; ?></textarea>
-    </div>
-    </div>
-        </div>
-        <?php
-    }
-    /**
-     * Save meta option in database
-     *
-     * @since 1.0.0
-     */
+	public function rex_slider_metabox() {
+		add_meta_box(
+			'rex_slider_mb',
+			'Rex Slider options',
+			array( $this, 'rex_slider_mb_form' ),
+			'rex_slides',
+			'low'
+		);
+	}
+	/**
+	 * Adds form fields to meta box
+	 *
+	 * @since 1.0.0
+	 */
 
-    public function rex_slider_save_options( $post_id )
-    {
+	public function rex_slider_mb_form() {
 
-        if(isset($_POST['rex_heading'])) {
-            update_post_meta($post_id, 'rex_heading', sanitize_text_field($_POST['rex_heading']));
-        } else {
-            delete_post_meta($post_id, 'rex_heading');
-        }
+		global $post;
+		// Get Value of Fields From Database
+		$rex_heading    = get_post_meta( $post->ID, 'rex_heading', true );
+		$rex_subheading = get_post_meta( $post->ID, 'rex_subheading', true );
+		?>
+	<div class="row">
+		<div class="label"><b>Slider Heading</div>
+		<div class="fields">
+			<textarea style="width:50%" name="rex_heading"><?php echo $rex_heading; ?></textarea>
+	</div>
+	</div>
+	<br>
+	<div class="row">
+		<div class="label"><b>Slider Sub heading</b></div>
+		<div class="fields">
+			<textarea style="width:50%" name="rex_subheading"><?php echo $rex_subheading; ?></textarea>
+	</div>
+	</div>
+		</div>
+		<?php
+	}
+	/**
+	 * Save meta option in database
+	 *
+	 * @since 1.0.0
+	 */
 
-        if(isset($_POST['rex_subheading'])) {
-            update_post_meta($post_id, 'rex_subheading', sanitize_text_field($_POST['rex_subheading']));
-        } else {
-            delete_post_meta($post_id, 'rex_subheading');
-        }
+	public function rex_slider_save_options( $post_id ) {
 
-    }
+		if ( isset( $_POST['rex_heading'] ) ) {
+			update_post_meta( $post_id, 'rex_heading', sanitize_text_field( $_POST['rex_heading'] ) );
+		} else {
+			delete_post_meta( $post_id, 'rex_heading' );
+		}
 
-    /**
-     * Generates slider shortcode
-     *
-     * @since 1.0.0
-     */
-    public function rex_slider_shortcode($atts)
-    {
+		if ( isset( $_POST['rex_subheading'] ) ) {
+			update_post_meta( $post_id, 'rex_subheading', sanitize_text_field( $_POST['rex_subheading'] ) );
+		} else {
+			delete_post_meta( $post_id, 'rex_subheading' );
+		}
 
-        $atts = shortcode_atts(
-            array(
-            'slider' => ''
-            ), $atts
-        );
+	}
 
-        $slider = $atts['slider'];
+	/**
+	 * Generates slider shortcode
+	 *
+	 * @since 1.0.0
+	 */
+	public function rex_slider_shortcode( $atts ) {
 
-        $args = array(
-             'post_type'       => 'rex_slides',
-             'posts_per_page'  => -1,
-             'tax_query'       => array(
-                  array(
-                     'taxonomy' => 'rex_slider',
-                     'field'    => 'id',
-                     'terms'    => $slider,
-                  ),
-             ),
-        );
+		$atts = shortcode_atts(
+			array(
+				'slider' => '',
+			),
+			$atts
+		);
 
-        $slides = new WP_Query($args);
+		$slider = $atts['slider'];
 
-        $slider= '<div class="flexslider">
+		$args = array(
+			'post_type'      => 'rex_slides',
+			'posts_per_page' => -1,
+			'tax_query'      => array(
+				array(
+					'taxonomy' => 'rex_slider',
+					'field'    => 'id',
+					'terms'    => $slider,
+				),
+			),
+		);
+
+		$slides = new WP_Query( $args );
+
+		$slider = '<div class="flexslider">
 			 <ul class="slides">';
 
-        if ($slides->have_posts()) : while ($slides->have_posts()) : $slides->the_post();
-                $post_id = get_the_ID();
-                $img= get_the_post_thumbnail($post_id, 'full');
-                $heading = get_post_meta($post_id, 'rex_heading', true);
-                $subheading = get_post_meta($post_id, 'rex_subheading', true);
-
-                $slider.='<li>'.$img.'
-									<h3 class="rex-slider-heading">'.$heading.'</h3>
-									<p class="rex-slider-caption">'.$subheading.'</p>
+		if ( $slides->have_posts() ) :
+			while ( $slides->have_posts() ) :
+				$slides->the_post();
+				$post_id    = get_the_ID();
+				$img        = get_the_post_thumbnail( $post_id, 'full' );
+				$heading    = get_post_meta( $post_id, 'rex_heading', true );
+				$subheading = get_post_meta( $post_id, 'rex_subheading', true );
+				if ( ! empty( $img ) ) :
+						$slider .= '<li>' . $img . '
+									<h3 class="rex-slider-heading">' . $heading . '</h3>
+									<p class="rex-slider-caption">' . $subheading . '</p>
 									</li>';
 
-        endwhile;
-        endif; wp_reset_query();
-        $slider.= '</ul>
+							endif;
+		endwhile;
+		endif;
+		wp_reset_query();
+		$slider .= '</ul>
 			 </div>';
 
-        return $slider;
+		return $slider;
 
-
-    }
+	}
 
 }
